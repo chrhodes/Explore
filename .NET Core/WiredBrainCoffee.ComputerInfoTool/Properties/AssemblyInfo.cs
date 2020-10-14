6a4ff36c60dd9363bd05241ad42c43f1be57c3da
Christopher Rhodes <chrhodes@cox.net>
2020-10-13 17:25:55 -0700

    Starting .NET F to .NET Core

---
+++ .NET Core/WiredBrainCoffee.ComputerInfoTool/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WiredBrainCoffee.ComputerInfoTool")]
+[assembly: AssemblyDescription("Tool that shows info of a computer")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Wired Brain Coffee")]
+[assembly: AssemblyProduct("WiredBrainCoffee.ComputerInfoTool")]
+[assembly: AssemblyCopyright("Copyright © Wired Brain Coffee 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7940ab13-0615-477c-9fa0-b51a1b2d40a7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ .NET Core/WiredBrainCoffee.ToDoList/DataProvider/TodoItemDataProvider.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using WiredBrainCoffee.ToDoList.Model;
+
+namespace WiredBrainCoffee.ToDoList.DataProvider
+{
+  public class TodoItemDataProvider
+  {
+    private readonly string _fileName;
+
+    public TodoItemDataProvider()
+    {
+      _fileName = ConfigurationManager.AppSettings["JsonFileName"];
+    }
+
+    public IEnumerable<TodoItem> LoadItems()
+    {
+      IEnumerable<TodoItem> todoItems = null;
+      if (File.Exists(_fileName))
+      {
+        var json = File.ReadAllText(_fileName);
+        todoItems = JsonConvert.DeserializeObject<List<TodoItem>>(json);
+      }
+
+      return todoItems ?? Enumerable.Empty<TodoItem>();
+    }
+
+    public void SaveItems(IEnumerable<TodoItem> items)
+    {
+      var listOfItems = items.ToList();
+
+      var json = JsonConvert.SerializeObject(listOfItems);
+      File.WriteAllText(_fileName, json);
+    }
+  }
+}
