8582eb2c7d99849fc88d7c3e78083228f4d41034
Christopher Rhodes <chrhodes@cox.net>
2021-03-06 10:46:30 -0800

    More diagrams

---
+++ Visual Studio/SDK Style Projects/ClassLibrary-Net Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ClassLibrary_Net_Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ Visual Studio/SDK Style Projects/ClassLibrary-Net Standard/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ClassLibrary_Net_Standard
+{
+    public class Class1
+    {
+    }
+}
